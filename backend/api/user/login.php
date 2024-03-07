7d8eabfbd3ba5f0c9607a285fcf9705e49a0cf45
Chetra07 <povchetra2222@gmail.com>
2024-03-07 18:34:48 +0700

    make a login

--- backend/api/user/login.php
+++ backend/api/user/login.php
@@ -1,21 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
+
 session_start();
-// include "conection.php";
+
 include "../config/conection.php";
 
-    if(empty($_SESSION['username'])){
-        // echo  $_SESSION['username'];
-
-    }else{
-        header("Location: home.php");
-    }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['username']) && !empty($_POST['username']) &&  isset($_POST['password']) && !empty($_POST['password'])) {
+    if (isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password'])) {
 
         $username = $_POST['username'];
         $password = $_POST['password'];
@@ -24,40 +18,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $username = $conn->real_escape_string($username);
         $password = $conn->real_escape_string($password);
 
-
         $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
         $rs = $conn->query($sql);
-        if ($rs) {
+        if ($rs && $rs->num_rows > 0) {
             $row = $rs->fetch_assoc();
             $_SESSION['username'] = $row['username'];
             $_SESSION['id'] = $row['id'];
-            header("Location: home.php");
+            echo json_encode(array("success" => true, "message" => "Login successful"));
+            exit;
+        } else {
+            echo json_encode(array("success" => false, "message" => "Invalid username or password"));
+            exit;
         }
+    } 
+    else {
+        echo json_encode(array("success" => false, "message" => "Missing username or password"));
+        exit;
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-
-<body>
-
-    <form action="" method="post">
-        <label for="username">username</label>
-        <input type="text" name="username" id="username">
-        <label for="password">password</label>
-        <input type="text" name="password" id="password">
-        <input type="submit" value="login">
-
-    </form>
-    Click here to clean <a href="register.php" tite="Logout">Register.
-
-</body>
-
-</html>
